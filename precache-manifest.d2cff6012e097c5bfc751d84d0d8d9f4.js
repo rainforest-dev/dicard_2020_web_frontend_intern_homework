e3421c3eecbe85309b6b571f1e06b1529e8cc6fb
Rainforest <rainforestnick@gmail.com>
2020-04-13 02:49:16 +0800

    deploy @ 2020-04-13T02:49:16.3N+0800

---
+++ precache-manifest.d2cff6012e097c5bfc751d84d0d8d9f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08923356b47721fe1b238ecbe4fc306e",
+    "url": "/dicard_2020_web_frontend_intern_homework/index.html"
+  },
+  {
+    "revision": "32b3e16c6e90e2b3b569",
+    "url": "/dicard_2020_web_frontend_intern_homework/static/css/main.32411314.chunk.css"
+  },
+  {
+    "revision": "bb16350eb226d3821ef7",
+    "url": "/dicard_2020_web_frontend_intern_homework/static/js/2.fba63b49.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dicard_2020_web_frontend_intern_homework/static/js/2.fba63b49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32b3e16c6e90e2b3b569",
+    "url": "/dicard_2020_web_frontend_intern_homework/static/js/main.30c31f3d.chunk.js"
+  },
+  {
+    "revision": "8d61748ab4c1ee383e94",
+    "url": "/dicard_2020_web_frontend_intern_homework/static/js/runtime-main.6c9a0171.js"
+  }
+]);
